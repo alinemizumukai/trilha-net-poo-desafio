1cf473fe524a4fb3d856c786a109f308b09374a2
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2023-10-29 18:49:27 -0300

    feat: implementação de testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n.::::: Testes com Smartphone Nokia :::::.\n");
+
+Smartphone smartNokia = new Nokia("16999991111", "G60", "12345678964336", 128);
+smartNokia.Ligar();
+smartNokia.InstalarAplicativo("Instagram");
+smartNokia.ReceberLigacao();
+
+Console.WriteLine("\n\n.::::: Testes com Smartphone iPhone :::::.\n");
+
+Smartphone smartIphone = new Iphone("16999992222", "15 Pro Max", "14569825646426", 512);
+smartIphone.Ligar();
+smartIphone.InstalarAplicativo("Whatsapp");
+smartIphone.ReceberLigacao();
+
+Console.WriteLine("\n.:::::::::::::::::: Fim ::::::::::::::::::.\n");
